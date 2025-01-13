cca0ba0e660d0adbe4ed91a94978069ae6366860
Hugo Cruz <hugocruzlfc@gmail.com>
2025-01-13 13:17:57 +0100

    feat: add use cat list without context

--- src/hooks/use-cat-list.ts
+++ src/hooks/use-cat-list.ts
@@ -1,33 +1,36 @@
-import { useContext, useMemo } from "react";
-import { CatContext } from "../context/cat-context";
+import { useState } from "react";
 import { Cat } from "../types";
 import { v4 as uuidv4 } from "uuid";
+import maineCoonImage from "../assets/images/maine_coon.avif";
+import siameseImage from "../assets/images/siamese.avif";
 
 const useCatList = () => {
-  const context = useContext(CatContext);
-  if (!context) {
-    throw new Error("useCatList must be used within a CatProvider");
-  }
+  const initialCats = [
+    {
+      id: uuidv4(),
+      breed: "Maine Coon",
+      image: maineCoonImage,
+      country: "USA",
+    },
+    {
+      id: uuidv4(),
+      breed: "Siamese",
+      image: siameseImage,
+      country: "Thailand",
+    },
+  ];
 
-  const { cats, setCats } = context;
+  const [cats, setCats] = useState(initialCats);
 
   const addCat = (cat: Cat) => {
     const newCatWithId = {
       ...cat,
       id: uuidv4(),
     };
-    setCats([...cats, newCatWithId]);
+    setCats((prevCats) => [...prevCats, newCatWithId]);
   };
 
-  const value = useMemo(
-    () => ({
-      cats,
-      addCat,
-    }),
-    [cats]
-  );
-
-  return value;
+  return { cats, addCat };
 };
 
 export default useCatList;
