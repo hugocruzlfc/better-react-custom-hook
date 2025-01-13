0e2190c878d970c1cf65c563c3aa02c642d9bf61
Hugo Cruz <hugocruzlfc@gmail.com>
2025-01-13 14:14:20 +0100

    refactor: update theme context ando hook

--- src/context/theme-context.tsx
+++ src/context/theme-context.tsx
@@ -1,14 +1,28 @@
-import { createContext, ReactNode, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { ThemeContextType } from "../types";
 
 export const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
 
-  const toggleTheme = () => {
+  useEffect(() => {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    setTheme(currentTheme);
+  }, []);
+
+  const toggleTheme = useCallback(() => {
+    localStorage.setItem("theme", theme === "light" ? "dark" : "light");
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
